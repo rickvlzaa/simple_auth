003c4c0be9fd4ab9b1ce66c745a8fdd7bc16b5ad
Rick Veloz <veloz.ricardo.a@gmail.com>
2018-05-10 16:16:21 -0500

    add index.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
-import path from 'path';
+import path from 'path'
 
 const app = express();
 
